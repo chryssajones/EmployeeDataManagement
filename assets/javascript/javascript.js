a2a5fa6de4e775b60c1568b43522521a5c605c0b
Jordan Lapid <jordanlapid@gmail.com>
2017-09-14 20:08:24 -0700

    javascript

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -6,9 +6,35 @@
     storageBucket: "",
     messagingSenderId: "476499000730"
   };
+
   firebase.initializeApp(config);
 
   var database = firebase.database();
 
+	var employee = "";
+	var role = "";
+	var startDate = "";
+	var monthlyRate = "";
+	var monthsWorked = "";
+	var totalBilled = "";
 
- 
+$("#addUser").on("click", function(event){
+     event.preventDefault();
+
+   // Grabbed values from text boxes
+      employee = $("#name").val().trim();
+      role = $("#role").val().trim();
+      startDate = $("#start").val().trim();
+      monthlyRate = $("#rate").val().trim();
+      monthsWorked = $("#months").val().trim();
+      totalBilled = $("#billed").val().trim();
+
+// Code for handling the push
+      database.ref("/users").push({
+        employee: employee,
+        role: role,
+        startDate: startDate,
+        monthlyRate: monthlyRate,
+        monthsWorked: monthsWorked,
+        totalBilled: totalBilled
+});

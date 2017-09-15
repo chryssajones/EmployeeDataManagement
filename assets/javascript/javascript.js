38dcd9ae1427ccba4c3bcdf8e843e461e8814270
Jordan Lapid <jordanlapid@gmail.com>
2017-09-14 19:45:15 -0700

    updates

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,14 @@
+  var config = {
+    apiKey: "AIzaSyBsmd71nuZWhANWNP0KMDhhKrwixo1rOVc",
+    authDomain: "employeedatabase-fdca6.firebaseapp.com",
+    databaseURL: "https://employeedatabase-fdca6.firebaseio.com",
+    projectId: "employeedatabase-fdca6",
+    storageBucket: "",
+    messagingSenderId: "476499000730"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+ 
